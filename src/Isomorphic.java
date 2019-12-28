ec53bb8ac7d9d98cdd41cb857954815258ad4b22
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2019-12-28 13:23:45 +0800

    empty message

--- src/Isomorphic.java
+++ src/Isomorphic.java
@@ -1,8 +1,11 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Isomorphic {
 
     public boolean check(String one, String another) {
         //a - b is true
-        if (one.length() == another.length()) {
+        if (Arrays.equals(IntStream.range(0, one.length()).toArray(), IntStream.range(0, another.length()).toArray())) {
             return true;
         }
         return false;
