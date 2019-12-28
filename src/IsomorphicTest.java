d096a54bf84c4ac675e4744836d2fd68af89afa8
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2019-12-28 13:10:22 +0800

    empty message

--- src/IsomorphicTest.java
+++ src/IsomorphicTest.java
@@ -1,9 +1,13 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class IsomorphicTest {
 
     @Test
-    public void xxx() {
-
+    public void should_be_false_when_length_is_different() {
+        Isomorphic isomorphic = new Isomorphic();
+        boolean actual = isomorphic.check("a", "bc");
+        assertFalse(actual);
     }
 }
